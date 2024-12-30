f4443f1fbb3a65e8fbfbbda753e7a1b1fc133aeb
Kateryna2882 <govoryhina82@gmail.com>
2024-12-30 19:57:12 +0200

    add fields

--- src/PositiveNumber.java
+++ src/PositiveNumber.java
@@ -1,5 +1,27 @@
-public class PositiveNumber {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class PositiveNumber {
+    public static void main(String[] args)  {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter three integers:");
+
+            int num1 = Integer.parseInt(reader.readLine());
+            int num2 = Integer.parseInt(reader.readLine());
+            int num3 = Integer.parseInt(reader.readLine());
+
+
+            int positiveCount = 0;
+
+            if (num1 > 0) positiveCount++;
+            if (num2 > 0) positiveCount++;
+            if (num3 > 0) positiveCount++;
+
+            System.out.println(positiveCount);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 }
