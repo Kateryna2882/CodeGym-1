2056f7d8000a0ae22e22b80d95b90ceee74c2381
Kateryna2882 <govoryhina82@gmail.com>
2025-01-01 16:15:27 +0200

    add solution

--- src/tasksAboutLoops/EvenNumbers.java
+++ src/tasksAboutLoops/EvenNumbers.java
@@ -2,6 +2,10 @@ package tasksAboutLoops;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-
+//        int number = 1;
+        for (int i = 2; i <= 100; i+=2) {
+//            if (number % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
-}
