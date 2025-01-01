ba44314285e0e5397bbaef69b30b1b029619d3ea
Kateryna2882 <govoryhina82@gmail.com>
2025-01-01 14:05:42 +0200

    add solution

--- src/tasksAboutLoops/SeeingDollars.java
+++ src/tasksAboutLoops/SeeingDollars.java
@@ -2,6 +2,12 @@ package tasksAboutLoops;
 
 public class SeeingDollars {
     public static void main(String[] args) {
+        String n = "$$$$$$$$$$";
+        int i = 10;
+        while (i > 0){
+            System.out.println(n);
+            i--;
 
+        }
     }
 }
