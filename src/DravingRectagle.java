4bcb71bc12e6b7b9144bce84c83373fcbec945a2
Kateryna2882 <govoryhina82@gmail.com>
2025-01-01 16:28:50 +0200

    add solution

--- src/DravingRectagle.java
+++ src/DravingRectagle.java
@@ -1,5 +1,23 @@
-public class DravingRectagle {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class DravingRectagle {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Use the keyboard to enter  number m");
+        int m = Integer.parseInt(reader.readLine());
+
+        System.out.println("Use the keyboard to enter  number n");
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                System.out.println("8");
+
+            }
+            System.out.println();
+
+        }
     }
 }
