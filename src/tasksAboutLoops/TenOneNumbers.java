82fe401e0d6d0ebf164b2d201ccbf7926e5809e1
Kateryna2882 <govoryhina82@gmail.com>
2025-01-01 13:11:18 +0200

    add TenOneNumbers class and solution

---
+++ src/tasksAboutLoops/TenOneNumbers.java
@@ -0,0 +1,11 @@
+package tasksAboutLoops;
+
+public class TenOneNumbers {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
