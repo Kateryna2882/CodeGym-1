5dec18eafd9a95c45e5e9e93378102801663c4e9
Kateryna2882 <govoryhina82@gmail.com>
2025-01-05 13:09:22 +0200

    add solution

--- src/tasksAboutLoops/DecentPay.java
+++ src/tasksAboutLoops/DecentPay.java
@@ -1,4 +1,9 @@
 package tasksAboutLoops;
 
 public class DecentPay {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 99; i++) {
+            System.out.println(i + 1 + ". I will never work for peanuts. Amigo");
+        }
+    }
 }
